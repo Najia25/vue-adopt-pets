91dc504cdb6cf782cfb65b59ea63647cb6045fd7
Najia Afrin <najiaafrin2@gmail.com>
2019-11-18 11:53:06 +0600

    data fetched and displayed in table

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    Cats
+    <b-table striped hover :items="cats"></b-table>
   </div>
 </template>
+
+<script>
+// @ is a shortcut for src directory, used instead of ../../
+import cats from '@/data/cats'
+
+export default {
+  data() {
+    return {
+      // cats:cats array that we are importing..since both have same name,shortcut would be just cats
+      cats
+    }
+  }
+}
+</script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    Dogs
+    <b-table striped hover :items="dogs"></b-table>
   </div>
 </template>
+
+<script>
+import dogs from '@/data/dogs'
+export default {
+	data() {
+		return {
+			dogs
+		}
+	}
+}
+</script>
