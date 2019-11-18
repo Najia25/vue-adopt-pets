d50745f861c078b04a8e738a966ac4ede92e0e5b
Najia Afrin <najiaafrin2@gmail.com>
2019-11-18 16:00:07 +0600

    data displayed in table, data stored in store.js, animal detail displayed on pet page

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <b-table striped hover :items="cats"></b-table>
+    <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats">
+      <template v-slot:cell(name)="data">
+      <!-- `data.value` is the value after formatted by the Formatter -->
+      <router-link :to="`/pets/cats/${data.index}`">
+        {{ data.value }}
+      </router-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
 // @ is a shortcut for src directory, used instead of ../../
-import cats from '@/data/cats'
+import { mapState } from 'vuex'
 
 export default {
-  data() {
-    return {
-      // cats:cats array that we are importing..since both have same name,shortcut would be just cats
-      cats
-    }
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
   }
 }
 </script>
--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,41 @@
 <template>
   <div>
-    Pet
+    <!-- {{ pet().name }} -->
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    // cats () {
+    //   return this.$store.state.cats
+    // }
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  // methods: {
+  //   pet () {
+  //     //this.cats
+  //     //this['cats']
+  //     const animal = this[this.$route.params.species][this.$route.params.id]
+  //     return animal
+  //   }
+  // }
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
